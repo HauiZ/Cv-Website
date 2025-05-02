7403fdc50b69e1dda3d925a21bab0f7709a9cfbb
truong <baotruong11298@gmail.com>
2025-05-02 16:06:27 +0700

    done admin search comming soon

--- src/pages/Admin/DashBoard/Header.jsx
+++ src/pages/Admin/DashBoard/Header.jsx
@@ -29,8 +29,9 @@ const Header = ({ setSelectedPage }) => {
         <div className="relative">
           <input
             type="text"
-            placeholder="Search"
+            placeholder="Comming Soon"
             className="pl-10 pr-3 py-1 bg-[#c5c7cb] [#7c7878] rounded border focus:outline-none text-black transition-all duration-200 hover:ring-2 hover:ring-blue-300 hover:bg-white focus:bg-white"
+            disabled
           />
           <FontAwesomeIcon icon={faSearch} className="absolute left-3 top-2 text-gray-500" />
         </div>

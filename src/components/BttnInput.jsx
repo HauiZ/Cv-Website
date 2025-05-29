6984e2059276fe954f18502c2ad55341299c3af1
truong <baotruong11298@gmail.com>
2025-05-29 21:42:52 +0700

    add show password in button

--- src/components/BttnInput.jsx
+++ src/components/BttnInput.jsx
@@ -1,32 +1,62 @@
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import { faEnvelope } from "../utils/fontAwsomeLib"; // default icon
+import React, { useState } from "react";
+import { faEnvelope, faEye, faEyeSlash } from "../utils/fontAwsomeLib";
 
-const Input = React.forwardRef(({ title, type, icon, errors, ...rest }, ref) => {
-  const name = rest.name; // name sẽ được lấy từ register()
+const Input = React.forwardRef(
+  ({ title, type, icon, errors, ...rest }, ref) => {
+    const name = rest.name;
+    const [showPassword, setShowPassword] = useState(false);
+    const [inputValue, setInputValue] = useState("");
+    const isPassword = type === "password";
 
-  return (
-    <>
-      <label htmlFor={name} className="block mb-2">{title}</label>
-      <div className="relative mb-[4vh]">
-        <span className="absolute left-3 top-1/2 transform -translate-y-1/2">
-          <FontAwesomeIcon
-            icon={icon}
-            className="text-green-500"
+    const handleChange = (e) => {
+      setInputValue(e.target.value);
+      if (rest.onChange) rest.onChange(e); // để không mất onChange từ register
+    };
+
+    return (
+      <>
+        <label htmlFor={name} className="block mb-2">
+          {title}
+        </label>
+        <div className="relative mb-[4vh]">
+          {/* Icon trái */}
+          <span className="absolute left-3 top-1/2 transform -translate-y-1/2">
+            <FontAwesomeIcon icon={icon} className="text-green-500" />
+          </span>
+
+          {/* Input */}
+          <input
+            type={isPassword ? (showPassword ? "text" : "password") : type}
+            className="bg-yellow border rounded w-full h-[5vh] pl-10 pr-10 min-h-[40px] max-w-[500px] max-sm:min-h-[50px]"
+            ref={ref}
+            {...rest}
+            onChange={handleChange}
           />
-        </span>
-        <input
-          type={type}
-          className="bg-yellow border rounded w-full h-[5vh] pl-10 pr-3 min-h-[40px] max-w-[500px] max-sm:min-h-[50px]"
-          ref={ref}
-          {...rest} // chứa name, onChange, ref,... từ register()
-        />
-      </div>
-      {errors?.[name] && <p className="text-red-500 mt-[-3vh]">{errors[name].message}</p>}
-    </>
-  );
-});
+
+          {/* Icon mắt bên phải */}
+          {isPassword && inputValue && (
+            <span
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
+              onClick={() => setShowPassword((prev) => !prev)}
+            >
+              <FontAwesomeIcon
+                icon={showPassword ? faEyeSlash : faEye}
+                className="text-green-500"
+              />
+            </span>
+          )}
+        </div>
+
+        {/* Hiển thị lỗi */}
+        {errors?.[name] && (
+          <p className="text-red-500 mt-[-3vh]">{errors[name].message}</p>
+        )}
+      </>
+    );
+  }
+);
 
 Input.displayName = "Input";
 

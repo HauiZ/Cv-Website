422bae1316f5c85be5d000805b71e45b568ef5cf
truong <baotruong11298@gmail.com>
2025-06-15 14:08:18 +0700

    fix bug logo search

--- src/pages/profileCompany/ListNews.jsx
+++ src/pages/profileCompany/ListNews.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import NewsItem from "./News"; // từ News.jsx
 
-export default function ListNews({ jobs }) {
+export default function ListNews({ jobs,logo }) {
   return (
     <div className="space-y-4">
       {jobs.map((job) => (
-        <NewsItem key={job.id} job={job} companyName={job?.companyName} logo={job?.logoUrl}/>
+        <NewsItem key={job.id} job={job} companyName={job?.companyName} logo={logo? logo : job?.logoUrl  }/>
       ))}
     </div>
   );

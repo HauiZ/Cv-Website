9b15f796a7084cb5f26cba60e11a09713a959e09
truong <baotruong11298@gmail.com>
2025-06-15 15:25:42 +0700

    fix outforcus searchbar admin

--- src/pages/Admin/SearchBar.jsx
+++ src/pages/Admin/SearchBar.jsx
@@ -2,9 +2,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { useSearch } from "../../contexts/SearchContext";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 export default function SearchBar({ contentDropdown, handleSubmit }) {
   const { searchTerm, setSearchTerm } = useSearch();
+  const [focused, setFocused] = useState(false);
   const location = useLocation();
   return (
     <div className="flex items-center space-x-3">
@@ -14,13 +16,19 @@ export default function SearchBar({ contentDropdown, handleSubmit }) {
           placeholder="Input"
           className="pl-10 pr-3 py-1 bg-[#c5c7cb] rounded border focus:outline-none text-black transition-all duration-200 hover:ring-2 hover:ring-blue-300 hover:bg-white focus:bg-white"
           value={searchTerm}
+          onBlur={() => {
+            setFocused(false);
+          }}
+          onFocus={() => {
+            setFocused(true);
+          }}
           onChange={(e) => setSearchTerm(e.target.value)}
           onKeyDown={(e) => {
             if (e.key === "Enter") {
               handleSubmit(searchTerm);
             }
           }}
-          disabled = {location.search !== "?page=overview"}
+          disabled={location.search !== "?page=overview"}
         />
         <FontAwesomeIcon
           icon={faSearch}
@@ -31,7 +39,7 @@ export default function SearchBar({ contentDropdown, handleSubmit }) {
         icon={faBell}
         className="text-white text-xl cursor-pointer hover:text-yellow-300 transition-all duration-200"
       />
-      {searchTerm && (
+      {searchTerm && focused && (
         <div className="absolute top-[2.75em] z-20">{contentDropdown}</div>
       )}
     </div>

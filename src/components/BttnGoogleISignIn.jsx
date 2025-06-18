d0d7c60e17629968aa0d013980a4fea14b3b5dba
truong <baotruong11298@gmail.com>
2025-06-18 18:35:04 +0700

    fix gg submit error

--- src/components/BttnGoogleISignIn.jsx
+++ src/components/BttnGoogleISignIn.jsx
@@ -9,7 +9,8 @@ export default function BttnGoogleISignIn() {
         class="flex items-center justify-center"
         onClick={handleGoogleOnClick}
       >
-        <button class="px-4 py-2 border flex gap-2 border-slate-500 rounded-lg text-slate-700 hover:border-slate-700 hover:text-slate-900 hover:shadow transition duration-150 w-50">
+        <button class="px-4 py-2 border flex gap-2 border-slate-500 rounded-lg text-slate-700 hover:border-slate-700 hover:text-slate-900 hover:shadow transition duration-150 w-50"
+        type="button">
           <img
             class="w-6 h-6"
             src="https://www.svgrepo.com/show/475656/google-color.svg"
